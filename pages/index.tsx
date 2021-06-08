688c8b81cba59552e73a41912db0bc4e13bf82aa
Guilherme Bruzzi <guibruzzi@gmail.com>
2021-06-08 13:51:50 -0300

    Add dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import Head from 'next/head'
 import useSwr from 'swr'
+import DarkModeToggle from 'react-dark-mode-toggle'
 
 import styles from '../styles/Home.module.css'
 import type { Answer } from '../utils/answer'
@@ -8,6 +9,8 @@ import type { Answer } from '../utils/answer'
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export default function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(() => true)
+
   const { data, error } = useSwr<{ answer: Answer }>('/api/answer', fetcher)
 
   if (error) return <div>Failed to load, try to refresh</div>
@@ -15,26 +18,46 @@ export default function Home() {
   const answer: Answer = data?.answer ?? 'NOTHING'
 
   return (
-    <div className={styles.container}>
+    <Fragment>
       <Head>
         <title>We have daily today?</title>
         <meta name="description" content="For the Physical Stores Team" />
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>We have daily today?</h1>
+      <div
+        className={`${styles.container} ${
+          isDarkMode ? styles.darkBackground : ''
+        }`}
+      >
+        <div className={styles.darkToggle}>
+          <DarkModeToggle
+            onChange={setIsDarkMode}
+            checked={isDarkMode}
+            size={80}
+          />
+        </div>
 
-        {answer === 'NOTHING' ? null : (
-          <h2
-            className={`${styles.description} ${
-              answer === 'NO' ? styles.descriptionNo : ''
+        <main className={styles.main}>
+          <h1
+            className={`${styles.title} ${
+              isDarkMode ? styles.colorInDark : ''
             }`}
           >
-            {answer}
-          </h2>
-        )}
-      </main>
-    </div>
+            We have daily today?
+          </h1>
+
+          {answer === 'NOTHING' ? null : (
+            <h2
+              className={`${styles.description} ${
+                answer === 'NO' ? styles.descriptionNo : ''
+              }`}
+            >
+              {answer}
+            </h2>
+          )}
+        </main>
+      </div>
+    </Fragment>
   )
 }
